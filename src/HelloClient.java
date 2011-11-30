1f839838dd4617f6c2201cbcb27ab1bc5f8dc40b
mrclrnt <marclaurent89@gmail.com>
2011-11-30 11:34:54 +0100

    Creation HelloClient

---
+++ src/HelloClient.java
@@ -0,0 +1,14 @@
+import java.rmi.*;
+
+public class HelloClient {
+	public static void main(String args[]) {
+    try {
+      // Récupération d'un stub sur l'objet serveur.
+      Hello obj = (Hello) Naming.lookup("//ma_machine/mon_serveur");
+      // Appel d'une méthode sur l'objet distant.
+      obj.sayHello();
+    } catch (Exception exc) {
+    	System.out.println("ERREUR");
+    }
+  }
+}
--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Plop");
+		System.out.println("Pllllllop");
 	}
 
 }
