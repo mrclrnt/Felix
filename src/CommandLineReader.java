f4a183600dc74548a4c434af085a2e6813ceb38b
Nicolas <nicolas.bouillon@eleves.ec-nantes.fr>
2011-12-09 11:01:13 +0100

    create commandLineReader class

---
+++ src/CommandLineReader.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+
+public class CommandLineReader {
+
+	private String function;
+	private String argument;
+	
+	public String getFunction() {
+		return function;
+	}
+
+	public String getArgument() {
+		return argument;
+	}
+
+	public CommandLineReader() {
+		System.out.println("Enter a function, please");
+		readCommandLine();
+	}
+
+	public void readCommandLine () {
+		Scanner scanner = new Scanner(System.in);
+		this.function = scanner.next();
+		this.argument = scanner.next();
+		System.out.println(function + " " + argument);
+		
+	}
+}
